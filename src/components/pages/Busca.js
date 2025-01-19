142105540e2ca8ed4d47de27980e5d297fd68877
joaovictortvs <tavaressjoaoo09@gmail.com>
2025-01-19 17:25:41 -0300

    Mudança de API e nova forma de aquisição para fazer

--- src/components/pages/Busca.js
+++ src/components/pages/Busca.js
@@ -4,12 +4,17 @@ import styles from './Busca.module.css'
 
 function Busca(){
 
+    async function BuscarCodigoFipe(codigo){
+        
+        //https://deividfortuna.github.io/fipe/
+    }
+
     return(
         <div className={styles.busca}>
             <div className={styles.containerBusca}>
                 <div className={styles.pesquisaFipe}>
                     <SlMagnifier className={styles.lupa}/>
-                    <input type="number" name="pesquisaCodigo" placeholder="Busque pelo código FIPE do veículo"/>
+                    <input type="number" name="pesquisaCodigo" placeholder="Busque pelo código FIPE do veículo" onKeyDown={(evt)=>{ if(evt.key === "Enter"){BuscarCodigoFipe(evt.target.value)}}}/>
                 </div>
                 <p className={styles.textExp}>*o código Fipe considera o ano e o mês de fabricação, além de outros fatores como modelo, versão e combustível, para identificar o veículo.</p>
             </div>
