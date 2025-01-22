8a2c56b2bc89276b8e4907f78919a84f34cc73f9
joaovictortvs <tavaressjoaoo09@gmail.com>
2025-01-21 22:32:40 -0300

    Consulta de veículo pronta, faltando estilizar e passar a configuração pras demais seções

--- src/components/pages/Carros.js
+++ src/components/pages/Carros.js
@@ -9,11 +9,65 @@ function Carros(){
 
     const [options, setOptions] = useState([])
     const [marcaClicada, setMarcaClick] = useState(false)
+
     const [modelos, setModelos] = useState([])
+    const [modeloClicado, setModeloClick] = useState(false)
+
+    const [anos, setAnos] = useState([])
+    const [anoClicado, setAnoClick] = useState(false)
+
+    const [marcaVeiculo, setMarcaVeiculo] = useState(null)
+    const [modeloVeiculo, setModeloVeiculo] = useState(null)
+    const [valorCarro, setValorCarro] = useState(null)
+    const [combustivel, setCombustivel] = useState(null)
+    const [anoModelo, setAnoModelo] = useState(null)
+    const [mesRef, setMesRef] = useState(null)
+
+    const [carroConsultado, setCarroConsultado] = useState(false)
+
+    function consultarCarro(){
+        if(marcaClicada == false || modeloClicado == false || anoClicado == false){
+            alert("É preciso todos os detalhes do veículo para consultar!")
+        } else{
+            fetch(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaClicada}/modelos/${modeloClicado}/anos/${anoClicado}`)
+            .then(resp=> resp.json())
+            .then((data)=>{
+                setMarcaVeiculo(data.Marca)
+                setModeloVeiculo(data.Modelo)
+                setValorCarro(data.Valor)
+                setAnoModelo(data.AnoModelo)
+                setCombustivel(data.Combustivel)
+                setMesRef(data.MesReferencia)
+                setCarroConsultado(true)
+            })
+        }
+    }
 
     function marcaChecked(marcaClick){
+        setCarroConsultado(false)
         setMarcaClick(marcaClick)
     }
+
+    function modeloChecked(modeloClick){
+        setCarroConsultado(false)
+        setModeloClick(modeloClick)
+    }
+
+    function anoChecked(anoClick){
+        setCarroConsultado(false)
+        setAnoClick(anoClick)
+    }
+
+    useEffect(()=>{
+        if(modeloClicado){
+            fetch(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${marcaClicada}/modelos/${modeloClicado}/anos`)
+            .then(resp=> resp.json())
+            .then((data)=>{
+                setAnos(data)
+            })
+            .catch(err=> console.log(err))
+        }
+    },[modeloClicado])
 
     useEffect(()=>{
         if(marcaClicada){
@@ -39,6 +93,7 @@ function Carros(){
         .then((data)=>{
             setOptions(data)
         })
+        .catch(err=> console.log(err))
     }, [])
 
 
@@ -46,15 +101,25 @@ function Carros(){
          <div className={styles.busca}>
             <div className={styles.containerBusca}>
                 <div className={styles.selectContainer}>
-                    <Select options={options}  text="Marcas" marcaSelecionada={marcaChecked}/>
-                    <Select options={modelos} text="Modelos" disabled={marcaClicada ? false : true} />
+                    <Select options={options}  text="Marca" selecionado={marcaChecked}/>
+                    <Select options={modelos} text="Modelos" disabled={marcaClicada ? false : true} selecionado={modeloChecked}/>
                 </div>
                  
                 <div className={styles.selectContainer}>
-                    <Select options={options} text="Marcas"/>
-                    <Select options={options} text="Modelos"/>
+                    <Select options={anos} text="Ano" disabled={modeloClicado ? false : true} selecionado={anoChecked}/>
+                    {/* <Select options={options} text="Modelos"/> */}
                 </div>
-                <Button text="Consultar Carro"/>
+                {carroConsultado && (
+                    <div> 
+                        <p>Marca: {marcaVeiculo}</p>
+                        <p>Modelo: {modeloVeiculo}</p>
+                        <p>Valor: {valorCarro}</p>
+                        <p>Ano-modelo: {anoModelo}</p>
+                        <p>Tipo combustível: {combustivel}</p>
+                        <p>Mês-referência: {mesRef}</p>
+                    </div>
+                )}
+                <Button text="Consultar Carro" consultar={consultarCarro}/>
             </div>
            
         </div>
--- src/components/pesquisaForm/Select.js
+++ src/components/pesquisaForm/Select.js
@@ -1,11 +1,11 @@
 import styles from './Select.module.css'
 
-function Select({options, text, marcaSelecionada, disabled}){
+function Select({options, text, selecionado, disabled}){
 
     return(
         <>
             <label htmlFor={text}>{text}</label>
-            <select id={text} key={text} disabled={disabled} onChange={(e)=>marcaSelecionada(e.target.value)}>
+            <select id={text} key={text} disabled={disabled} onChange={(e)=>selecionado(e.target.value)}>
             <option value="">Escolha uma opção</option>
             {options.map((option)=>(
                 <option key={option.codigo} value={option.codigo}>{option.nome}</option>
